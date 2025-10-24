9ceefa2ac326e0fa3cdfb397e5ffbbb88a77d031
Vladislav <morozvv75@gmail.com>
2025-10-24 19:01:08 +0300

    feat: made sum code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Input error");
+    let numbers: Vec<i32> = input
+        .trim()
+        .split_whitespace()
+        .map(|s| s.parse().expect("Parcing error"))
+        .collect();
+
+    let sum = numbers[0] + numbers[1];
+    println!("{}", sum);
 }
