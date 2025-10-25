bdaabcba2fc2aded3696824c17164fbdd0a755d8
Vladislav <morozvv75@gmail.com>
2025-10-25 20:47:00 +0300

    feat: implemented sum of two numbers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn main() {
-    let mut input = String::new();
+    let mut input: String = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
     let nums: Vec<i32> = input
